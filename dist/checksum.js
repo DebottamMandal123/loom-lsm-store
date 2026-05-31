0e1ad58b1ace0cb12b9ece8a90076f32df753411
Debottam Mandal <131802431+DebottamMandal123@users.noreply.github.com>
2026-06-01 00:49:17 +0530

    Add runnable checksum build

---
+++ dist/checksum.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function checksum32(data) {
+  const buffer = typeof data === "string" ? Buffer.from(data, "utf8") : data;
+  let hash = 2166136261 >>> 0;
+  for (const byte of buffer) {
+    hash ^= byte;
+    hash = Math.imul(hash, 16777619) >>> 0;
+  }
+  return hash >>> 0;
+}
+
+module.exports = { checksum32 };
