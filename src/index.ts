2a3d83c516c48c7bb520634469e1249a6b8aff60
Debottam Mandal <131802431+DebottamMandal123@users.noreply.github.com>
2026-05-31 19:18:47 +0530

    Add TypeScript public exports

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+export { BloomFilter } from "./bloom";
+export { checksum32 } from "./checksum";
+export { LoomStore } from "./store";
+export type {
+  BlockIndex,
+  Entry,
+  Manifest,
+  ScanOptions,
+  StoreOptions,
+  StoredValue,
+  StoreStats,
+  SstableMeta,
+  WalLine,
+  WalRecord,
+} from "./types";
